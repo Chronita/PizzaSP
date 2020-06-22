0db2fb7978c3b7f37c5263bbaa9f0fa3e2a1c80c
Katerina Smirnova <katerina.smirnova98@yandex.ru>
2020-06-22 18:29:38 +0300

    Add enum Ingredients

---
+++ src/main/java/com/epam/pizza/model/products/Ingredients.java
@@ -0,0 +1,17 @@
+package com.epam.pizza.model.products;
+
+public enum Ingredients {
+    CHICKEN,
+    HAM,
+    PEPPERONI,
+    TOMATO_SAUCE,
+    CHORIZA,
+    MOZZARELLA,
+    LARGE_PORTION_OF_MOZZARELLA,
+    MIX_OF_CHEDDAR_AND_PARMESAN,
+    BLUE_CHEESE,
+    CREAM_SAUCE,
+    CHAMPIGNONS,
+    GARLIC,
+    COTTAGE_CHEESE;
+    }
