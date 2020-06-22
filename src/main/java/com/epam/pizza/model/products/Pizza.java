4f82f5df20b9a3dc908474a651437f098fc0d16e
Katerina Smirnova <katerina.smirnova98@yandex.ru>
2020-06-22 19:24:48 +0300

    Fix class Pizza (добавила наследование от класса Product)

--- src/main/java/com/epam/pizza/model/products/Pizza.java
+++ src/main/java/com/epam/pizza/model/products/Pizza.java
@@ -2,14 +2,15 @@ package com.epam.pizza.model.products;
 
 import java.util.List;
 
-public class Pizza {
+public class Pizza extends Product{
 
     private String pizzaType;
     private String nutritionalValue;
     private int ccal;
     private List<Ingredients> ingredients;
 
-    public Pizza(String pizzaType, String nutritionalValue, int ccal, List<Ingredients> ingredients) {
+    public Pizza(String productName, int productValue, String pizzaType, String nutritionalValue, int ccal, List<Ingredients> ingredients) {
+        super(productName, productValue);
         this.pizzaType = pizzaType;
         this.nutritionalValue = nutritionalValue;
         this.ccal = ccal;
