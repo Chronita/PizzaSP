f00506ef2594417e33db85b8799495d70abc7e71
Nadia <nadezhdagahariya7@gmail.com>
2020-06-23 09:06:45 +0300

    Add list of Ingredients

---
+++ src/main/java/com/epam/pizza/model/products/Ingredients.java
@@ -0,0 +1,17 @@
+package com.epam.pizza.model.products;
+
+public enum Ingredients {
+    CHICKEN,
+    HAM,
+    PEPPERONI,
+    CHORIZO,
+    CHAMPIGNONS,
+    CHEESE_MOZZARELLA,
+    CHEESE_BLUE,
+    CHEESE_CHEDDAR,
+    CHEESE_PARMESAN,
+    TOMATO_SAUCE,
+    CREAM_SAUCE,
+    GARLIC_SAUCE,
+    COTTAGE_CHEESE_SAUCE
+}
