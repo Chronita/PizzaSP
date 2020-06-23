385cd6bf722a3b36455eb566290b3179b2896991
Nadia <nadezhdagahariya7@gmail.com>
2020-06-23 09:59:43 +0300

    Fix class Pizza

--- src/main/java/com/epam/pizza/model/products/Pizza.java
+++ src/main/java/com/epam/pizza/model/products/Pizza.java
@@ -2,18 +2,20 @@ package com.epam.pizza.model.products;
 
 import java.util.List;
 
-public class Pizza {
+public class Pizza extends Product {
     private String pizza_type;
     private String nutrition_value;
     private int calories;
     private List<Ingredients> ingredients;
 
-    public Pizza(String pizza_type, String nutrition_value, int calories, List<Ingredients> ingredients) {
+    public Pizza(String name, int price, String pizza_type, String nutrition_value, int calories, List<Ingredients> ingredients) {
+        super(name, price);
         this.pizza_type = pizza_type;
         this.nutrition_value = nutrition_value;
         this.calories = calories;
         this.ingredients = ingredients;
     }
+
 
     public String getPizza_type() {
         return pizza_type;
